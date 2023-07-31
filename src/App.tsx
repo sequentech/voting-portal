6ff8855e74bc8502a2496c30c392cad450dbd9d0
Findeton <felix@sequentech.io>
2023-07-31 11:17:26 -0500

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,16 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 import React, {useState} from "react"
 import {Routes, Route} from "react-router-dom"
-import NewBallotVerifierLib, {set_hooks} from "new-ballot-verifier-lib"
 import {styled} from "@mui/material/styles"
 import {Footer, Header, PageBanner} from "ui-essentials"
 import Stack from "@mui/material/Stack"
-import {IConfirmationBallot, provideBallotService} from "./services/BallotService"
 import {StartScreen} from "./screens/StartScreen"
-
-NewBallotVerifierLib().then(set_hooks)
 
 const StyledApp = styled(Stack)`
     min-height: 100vh;
 `
 
 const App = () => {
-    const [confirmationBallot, setConfirmationBallot] = useState<IConfirmationBallot | null>(null)
-    const [ballotId, setBallotId] = useState<string>("")
-    const [fileName, setFileName] = useState("")
-    const ballotService = provideBallotService()
 
     return (
         <StyledApp>
--- src/screens/StartScreen.tsx
+++ src/screens/StartScreen.tsx
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2022 Félix Robles <felix@sequentech.io>
 //
 // SPDX-License-Identifier: AGPL-3.0-only
-import React
+import React from "react"
 
-const StartScreen: React.FC = () => <div>Hello world</div>
+export const StartScreen: React.FC = () => <div>Hello world</div>
