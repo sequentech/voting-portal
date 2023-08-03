2d5921898e15cc0b35ae8963714556dec15136ab
Findeton <felix@sequentech.io>
2023-08-03 11:41:06 -0500

    wip

--- src/store/store.ts
+++ src/store/store.ts
@@ -4,6 +4,7 @@
 import {configureStore, ThunkAction, Action} from "@reduxjs/toolkit"
 import electionsReducer from "./elections/electionsSlice"
 import ballotSelectionsReducer from "./ballotSelections/ballotSelectionsSlice"
+import auditableBallotsReducer from "./auditableBallots/auditableBallotsSlice"
 
 // note: use Immer, https://immerjs.github.io/immer/
 
@@ -11,6 +12,7 @@ export const store = configureStore({
     reducer: {
         elections: electionsReducer,
         ballotSelections: ballotSelectionsReducer,
+        auditableBallots: auditableBallotsReducer,
     },
 })
 
